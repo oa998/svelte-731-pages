b4815b0265e482718b5c5633db3e999aa59b5568
zzz sss <onlineaccounts998@gmail.com>
2023-10-20 14:09:39 -0400

    auto commit

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,10 @@ export default {
 			fallback: undefined,
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: '/svelte-731-pages'
+		}
 	},
 	preprocess: vitePreprocess()
 };
