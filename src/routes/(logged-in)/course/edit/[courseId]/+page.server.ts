3aaaf9740c8f0d546b537bd232e6435d388aa041
zzz sss <onlineaccounts998@gmail.com>
2023-10-29 12:13:43 -0400

    auto commit

---
+++ src/routes/(logged-in)/course/edit/[courseId]/+page.server.ts
@@ -0,0 +1,18 @@
+import { redirect } from '@sveltejs/kit';
+
+const courses = { nails: {}, lashes: {} };
+
+export function load({ params }: { params: { courseId: keyof typeof courses } }) {
+	const courseId = params.courseId;
+	if (!courses[courseId]) {
+		throw redirect(307, '/');
+	}
+	return courses[courseId];
+}
+
+/** @type {import('./$types').EntryGenerator} */
+export function entries() {
+	return Object.keys(courses).map((courseId) => ({ courseId }));
+}
+
+export const prerender = true;
