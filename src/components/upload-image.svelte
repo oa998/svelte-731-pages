c11ab91f0d1cbaeb6dbc7ccf27bc7b12d8328b87
zzz sss <onlineaccounts998@gmail.com>
2023-11-04 13:40:23 -0400

    auto commit

--- src/components/upload-image.svelte
+++ src/components/upload-image.svelte
@@ -1,4 +1,24 @@
 <script lang="ts">
+	import { env } from '$env/dynamic/public';
+
+	const moveToAlbum = (imageHash: string) => {
+		const formdata = new FormData();
+		formdata.append('ids[]', imageHash);
+
+		var requestOptions = {
+			method: 'POST',
+			headers: {
+				Authorization: `Bearer ${env.PUBLIC_IMGUR_ACCESS_TOKEN}`
+			},
+			body: formdata,
+			redirect: 'follow'
+		};
+
+		fetch('https://api.imgur.com/3/album/p2M8FFhintt0aW5/add', requestOptions).catch((error) =>
+			console.log('error moving to album', error)
+		);
+	};
+
 	const upload = (
 		e: Event & {
 			readonly submitter: HTMLElement | null;
@@ -6,22 +26,22 @@
 			currentTarget: EventTarget & HTMLFormElement;
 		}
 	) => {
-		const form = new FormData(e.currentTarget);
-		console.log(form.get('album'), '<<album');
 		const requestOptions = {
 			method: 'POST',
 			headers: {
-				Authorization: `Client-ID 9f881d93c8b3282`
+				Authorization: `Client-ID ${env.PUBLIC_IMGUR_CLIENT_ID}`
 			},
 			body: new FormData(e.currentTarget),
 			redirect: 'follow'
 		};
 
-		// wqV6XuJ
-
 		fetch('https://api.imgur.com/3/image', requestOptions)
-			.then((response) => response.text())
-			.then((result) => console.log(result))
+			.then((response) => response.json())
+			.then((result) => {
+				console.log(JSON.stringify(result, null, 2));
+				const imgHash = result.data.id;
+				moveToAlbum(imgHash);
+			})
 			.catch((error) => console.log('error', error));
 	};
 </script>
