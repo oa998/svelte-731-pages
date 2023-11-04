877275fee6dfa4fb6f7257c96091e5ea50810133
zzz sss <onlineaccounts998@gmail.com>
2023-11-04 13:59:24 -0400

    auto commit

--- src/lib/image-upload.ts
+++ src/lib/image-upload.ts
@@ -2,18 +2,18 @@ import { env } from '$env/dynamic/public';
 
 const moveToAlbum = (imageHash: string) => {
 	const formdata = new FormData();
-	formdata.append('ids[]', imageHash);
+	formdata.append('deletehashes[]', imageHash);
 
 	const requestOptions = {
 		method: 'POST',
 		headers: {
-			Authorization: `Bearer ${env.PUBLIC_IMGUR_ACCESS_TOKEN}`
+			Authorization: `Client-ID ${env.PUBLIC_IMGUR_CLIENT_ID}`
 		},
 		body: formdata,
 		redirect: 'follow'
 	};
 
-	fetch('https://api.imgur.com/3/album/OBfO4UG/add', requestOptions).catch((error) =>
+	fetch('https://api.imgur.com/3/album/p2M8FFhintt0aW5/add', requestOptions).catch((error) =>
 		console.log('error moving to album', error)
 	);
 };
@@ -38,8 +38,30 @@ export const upload = (
 		.then((response) => response.json())
 		.then((result) => {
 			console.log(JSON.stringify(result, null, 2));
-			const imgHash = result.data.id;
-			moveToAlbum(imgHash);
+			moveToAlbum(result.data.deletehash);
 		})
 		.catch((error) => console.log('error', error));
 };
+
+/**
+ * 
+ * 
+{
+    "data": {
+        "id": "OBfO4UG",
+        "deletehash": "p2M8FFhintt0aW5",
+        "account_id": "3010605",
+        "title": "731vibes",
+        "description": "",
+        "privacy": "0",
+        "cover": "",
+        "order": "",
+        "layout": "b",
+        "num_images": "",
+        "datetime": "2023-11-04 17:20:40"
+    },
+    "success": true,
+    "status": 200
+}
+
+ */
