4484350d03a33957d20225d4e84ecc23de11a833
zzz sss <onlineaccounts998@gmail.com>
2023-11-05 14:15:36 -0500

    auto commit

--- src/lib/articles.ts
+++ src/lib/articles.ts
@@ -11,3 +11,27 @@ export function getAllChapters(courseId: string) {
 		.then(throwIfNot2xx(`Unable to read all chapters for ${courseId}`))
 		.then((r) => r.json());
 }
+
+export function getCourse(courseId: string) {
+	return fetch(
+		`https://nginx-anything-storage-glovbogi2a-uc.a.run.app/storage?q=course/${courseId}`,
+		{
+			method: 'GET',
+			credentials: 'include'
+		}
+	)
+		.then(throwIfNot2xx(`Unable to read course ${courseId}`))
+		.then((r) => r.json());
+}
+
+export function getChapter(courseId: string, chapterId: string) {
+	return fetch(
+		`https://nginx-anything-storage-glovbogi2a-uc.a.run.app/storage?q=course/${courseId}/chapter/${chapterId}`,
+		{
+			method: 'GET',
+			credentials: 'include'
+		}
+	)
+		.then(throwIfNot2xx(`Unable to read chapters ${chapterId} of course ${courseId}`))
+		.then((r) => r.json());
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
-	import { getAllChapters } from '$lib/articles';
+	import { getAllChapters, getChapter, getCourse } from '$lib/articles';
 	import { onMount } from 'svelte';
 
-	onMount(() => getAllChapters('lashes').then((r) => console.log({ r })));
+	onMount(() => {
+		getAllChapters('lashes').then((allChapters) => console.log({ allChapters }));
+		getCourse('lashes').then((course) => console.log({ course }));
+		getChapter('lashes', '2YzscsLXatdgDj1N0Eaj').then((chapter) => console.log({ chapter }));
+	});
 </script>
 
 <div class="text-blue-50 text-3xl">hello</div>
