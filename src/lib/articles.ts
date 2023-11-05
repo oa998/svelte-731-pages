0809a52b98dc911a18789a26546711ebec65304c
zzz sss <onlineaccounts998@gmail.com>
2023-11-05 15:53:47 -0500

    auto commit

--- src/lib/articles.ts
+++ src/lib/articles.ts
@@ -1,5 +1,16 @@
 import { throwIfNot2xx } from './fetch-utils';
 
+type Course = {
+	title: string;
+	markdown: string;
+};
+
+type Chapter = {
+	title: string;
+	markdown: string;
+	sequence: number;
+};
+
 export function getAllChapters(courseId: string) {
 	return fetch(
 		`https://nginx-anything-storage-glovbogi2a-uc.a.run.app/storage?q=course/${courseId}/chapter`,
@@ -9,7 +20,8 @@ export function getAllChapters(courseId: string) {
 		}
 	)
 		.then(throwIfNot2xx(`Unable to read all chapters for ${courseId}`))
-		.then((r) => r.json());
+		.then((r) => r.json())
+		.then((j) => j as Chapter[]);
 }
 
 export function getCourse(courseId: string) {
@@ -21,7 +33,12 @@ export function getCourse(courseId: string) {
 		}
 	)
 		.then(throwIfNot2xx(`Unable to read course ${courseId}`))
-		.then((r) => r.json());
+		.then((r) => r.json())
+		.then((j) => {
+			const courses = j as Course[];
+			if (courses.length > 0) return courses[0];
+			throw new Error(`Course ${courseId} does not exist.`);
+		});
 }
 
 export function getChapter(courseId: string, chapterId: string) {
@@ -33,5 +50,42 @@ export function getChapter(courseId: string, chapterId: string) {
 		}
 	)
 		.then(throwIfNot2xx(`Unable to read chapters ${chapterId} of course ${courseId}`))
+		.then((r) => r.json())
+		.then((j) => {
+			const chapters = j as Chapter[];
+			if (chapters.length > 0) return chapters[0];
+			throw new Error(`Chapter ${chapterId} from course ${courseId} does not exist.`);
+		});
+}
+
+export function postCourse(courseId: string, body: Course) {
+	return fetch(
+		`https://nginx-anything-storage-glovbogi2a-uc.a.run.app/storage?q=course/${courseId}`,
+		{
+			method: 'POST',
+			headers: {
+				['content-type']: 'application/json'
+			},
+			body: JSON.stringify({ ...body, courseId }),
+			credentials: 'include'
+		}
+	)
+		.then(throwIfNot2xx(`Unable to set course ${courseId}`))
+		.then((r) => r.json());
+}
+
+export function postChapter(courseId: string, chapterId: string, body: Chapter) {
+	return fetch(
+		`https://nginx-anything-storage-glovbogi2a-uc.a.run.app/storage?q=course/${courseId}/chapter/${chapterId}`,
+		{
+			method: 'POST',
+			headers: {
+				['content-type']: 'application/json'
+			},
+			body: JSON.stringify({ ...body, chapterId }),
+			credentials: 'include'
+		}
+	)
+		.then(throwIfNot2xx(`Unable to set chapter ${chapterId} of course ${courseId}`))
 		.then((r) => r.json());
 }
