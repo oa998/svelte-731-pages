99c0ad7b038bd8d80f7de868b30941701dc85989
zzz sss <onlineaccounts998@gmail.com>
2024-01-29 21:55:35 -0500

    auto commit

--- src/routes/(landing)/university/curriculum/+page.svelte
+++ src/routes/(landing)/university/curriculum/+page.svelte
@@ -1,9 +1,19 @@
-<div class="text-white max-w-2xl m-auto px-10 flex flex-col gap-28">
-	<div class="text-4xl font-sans font-bold">731 Vibes Lash University</div>
+<script>
+	import { base } from '$app/paths';
+	import CompanyName from '$components/company-name.svelte';
+</script>
 
-	<div class="text-2x font-sans">
-		Why choose 731 Vibes Lash University for your Eyelash License?
+<CompanyName />
+<div class="text-white max-w-2xl m-auto pt-10 px-10 flex flex-col gap-2">
+	<div class="text-4xl sm:text-6xl font-serif text-center font-extralight">Lash University</div>
+
+	<img src="/lash_university_crest.webp" alt="crest" class="max-w-xs m-auto" />
+
+	<div class="text-2xl text-center font-extralight">
+		Why choose <span class="font-normal">731 Vibes Lash University</span> for your Eyelash License?
 	</div>
+</div>
+<div class="text-white max-w-2xl m-auto py-20 px-10 flex flex-col gap-20">
 	<div>
 		<h1 class="pb-2 text-2xl sm:text-5xl font-sans font-thin">Comprehensive Curriculum</h1>
 		<p class="text-base sm:text-lg font-extrathin dark-text-shadow font-serif font-thin">
@@ -12,6 +22,65 @@
 				class="text-lime-400 font-light">master the skills</span
 			> needed to create captivating lash transformations.
 		</p>
+	</div>
+
+	<div>
+		<h1 class="pb-2 text-2xl sm:text-5xl font-sans font-thin">Experienced Instructors</h1>
+		<p class="text-base sm:text-lg font-extrathin dark-text-shadow font-serif font-thin">
+			Learn from industry experts and seasoned professionals who are dedicated to nurturing your
+			talent. Our <span class="text-lime-400 font-light"
+				>experienced instructors bring a wealth of knowledge</span
+			>, providing personalized guidance to help you excel in the art of lash extensions.
+		</p>
+	</div>
+
+	<div>
+		<h1 class="pb-2 text-2xl sm:text-5xl font-sans font-thin">Personalized Training</h1>
+		<p class="text-base sm:text-lg font-extrathin dark-text-shadow font-serif font-thin">
+			Benefit from personalized attention with our low student-to-instructor ratio. Our experienced
+			educators tailor their guidance to <span class="text-lime-400 font-light"
+				>your unique learning style</span
+			>, ensuring you grasp the intricacies of lash application and design.
+		</p>
+	</div>
+
+	<div>
+		<h1 class="pb-2 text-2xl sm:text-5xl font-sans font-thin">State-of-the-Art Facilities</h1>
+		<p class="text-base sm:text-lg font-extrathin dark-text-shadow font-serif font-thin">
+			Train in a <span class="text-lime-400 font-light">modern and well-equipped environment</span>,
+			utilizing cutting-edge tools and products. Our school is designed to mimic real-world salon
+			settings, giving you the confidence to excel in any professional setting.
+		</p>
+	</div>
+
+	<div>
+		<h1 class="pb-2 text-2xl sm:text-5xl font-sans font-thin">Hands-On Training</h1>
+		<p class="text-base sm:text-lg font-extrathin dark-text-shadow font-serif font-thin">
+			Practice makes perfect! Our hands-on training approach allows you to work on real clients,
+			honing your skills and <span class="text-lime-400 font-light"
+				>building the confidence needed to succeed</span
+			> in the competitive beauty industry.
+		</p>
+	</div>
+
+	<div>
+		<h1 class="pb-2 text-2xl sm:text-5xl font-sans font-thin">Continuous Support</h1>
+		<p class="text-base sm:text-lg font-extrathin dark-text-shadow font-serif font-thin">
+			Join a supportive community of students, alumni, and industry professionals. <span
+				class="text-lime-400 font-light">Our network offers ongoing support</span
+			>, collaboration opportunities, and a platform to share experiences, fostering a sense of
+			belonging in the world of lash artistry.
+		</p>
+	</div>
+
+	<hr />
+
+	<div class="text-xl text-center font-extralight">
+		Don't just dream about a career in lash artistry – make it a reality with <span
+			class="font-normal">731 Vibes Lash University</span
+		>. <a href={`${base}/university/downloads`} class="underline text-blue-300">Enroll today</a> and
+		take the first step towards unlocking your full potential as a licensed lash artist. Your journey
+		to mastering the art of lashes starts here!
 	</div>
 	<!-- Welcome to 731 Vibes Lash University – where beauty meets expertise, and dreams of becoming a skilled licensed lash artist come to life! At our esteemed lash university, we invite you to embark on an exciting journey that goes beyond the ordinary, shaping your passion for beauty into a thriving career.
 
