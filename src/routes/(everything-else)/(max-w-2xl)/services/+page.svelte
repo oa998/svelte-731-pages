6a105c08c0a2929a1ce12c71b7593c957867478c
zzz sss <onlineaccounts998@gmail.com>
2024-01-31 20:30:23 -0500

    auto commit

--- src/routes/(everything-else)/(max-w-2xl)/services/+page.svelte
+++ src/routes/(everything-else)/(max-w-2xl)/services/+page.svelte
@@ -62,43 +62,43 @@
 			<p>Embark on a journey of self-discovery and wellness with our Reiki sessions:</p>
 
 			<p>
-				<span class="font-light pr-1">Energy Alignment:</span> Experience the subtle yet profound effects
-				of energy alignment as our skilled Reiki practitioner channels healing energy to restore balance
-				within your body.
+				<span class="font-semibold pr-1">Energy Alignment:</span> Experience the subtle yet profound
+				effects of energy alignment as our skilled Reiki practitioner channels healing energy to restore
+				balance within your body.
 			</p>
 
 			<p>
-				<span class="font-light pr-1">Stress Relief:</span> Let go of tension and stress as the soothing
+				<span class="font-semibold pr-1">Stress Relief:</span> Let go of tension and stress as the soothing
 				energy of Reiki envelops you, promoting a deep sense of relaxation and tranquility.
 			</p>
 
 			<p>
-				<span class="font-light pr-1">Emotional Harmony:</span> Find emotional equilibrium as Reiki works
-				to release blockages and promote a sense of inner peace, allowing you to navigate life's challenges
-				with a centered mind.
+				<span class="font-semibold pr-1">Emotional Harmony:</span> Find emotional equilibrium as Reiki
+				works to release blockages and promote a sense of inner peace, allowing you to navigate life's
+				challenges with a centered mind.
 			</p>
 
 			<p>
-				<span class="font-light pr-1">Physical Wellbeing:</span> Enhance your overall physical wellbeing
+				<span class="font-semibold pr-1">Physical Wellbeing:</span> Enhance your overall physical wellbeing
 				as Reiki supports the body's natural healing processes, addressing imbalances and promoting a
 				sense of vitality.
 			</p>
 
 			<p>
-				<span class="font-light pr-1">Personalized Sessions:</span> Our experienced Reiki practitioners
+				<span class="font-semibold pr-1">Personalized Sessions:</span> Our experienced Reiki practitioners
 				tailor each session to your unique needs, ensuring a personalized and meaningful experience that
 				resonates with you.
 			</p>
 
 			<p>
-				<span class="font-light pr-1">Safe and Nurturing Environment:</span> Relax in a safe and nurturing
+				<span class="font-semibold pr-1">Safe and Nurturing Environment:</span> Relax in a safe and nurturing
 				environment where the energy flows freely, creating a space for healing and self-discovery.
 			</p>
 
 			<p>
 				<span
 					>Take the first step towards holistic wellness – book a Reiki session at <span
-						class="font-light">731 Vibes</span
+						class="font-semibold">731 Vibes</span
 					>. Allow the gentle touch of energy to guide you towards balance, harmony, and a renewed
 					sense of well-being. Embrace the healing power of Reiki and unlock the potential for a
 					healthier, more harmonious life.</span
@@ -157,38 +157,38 @@
 			</p>
 
 			<p>
-				<span class="font-light pr-1">Professional Expertise:</span> Trust in the hands of our experienced
+				<span class="font-semibold pr-1">Professional Expertise:</span> Trust in the hands of our experienced
 				professionals who utilize advanced techniques and industry-leading whitening products for safe
 				and effective results.
 			</p>
 
 			<p>
-				<span class="font-light pr-1">Customized Approach:</span> Our teeth whitening sessions are personalized
-				to your unique dental needs and preferences, ensuring a customized treatment that delivers optimal
-				results without compromising the health of your teeth.
+				<span class="font-semibold pr-1">Customized Approach:</span> Our teeth whitening sessions are
+				personalized to your unique dental needs and preferences, ensuring a customized treatment that
+				delivers optimal results without compromising the health of your teeth.
 			</p>
 
 			<p>
-				<span class="font-light pr-1">Quick and Convenient:</span> Achieve a brighter smile in just one
-				session, saving you time and providing immediate results. Our efficient teeth whitening process
+				<span class="font-semibold pr-1">Quick and Convenient:</span> Achieve a brighter smile in just
+				one session, saving you time and providing immediate results. Our efficient teeth whitening process
 				fits seamlessly into your busy schedule.
 			</p>
 
 			<p>
-				<span class="font-light pr-1">Safe and Gentle:</span> Rest easy knowing that our teeth whitening
+				<span class="font-semibold pr-1">Safe and Gentle:</span> Rest easy knowing that our teeth whitening
 				treatments are designed with your comfort in mind. We prioritize the health of your teeth and
 				gums, delivering stunning results without sensitivity concerns.
 			</p>
 
 			<p>
-				<span class="font-light pr-1">Confidence Boost:</span> Reveal a brighter, whiter smile that enhances
-				your overall appearance and boosts your self-confidence. Smile with pride and let your newfound
-				radiance shine.
+				<span class="font-semibold pr-1">Confidence Boost:</span> Reveal a brighter, whiter smile that
+				enhances your overall appearance and boosts your self-confidence. Smile with pride and let your
+				newfound radiance shine.
 			</p>
 
 			<p>
 				<span
-					>Uncover the brilliance of a whiter smile at <span class="font-light">731 Vibes</span>.
+					>Uncover the brilliance of a whiter smile at <span class="font-semibold">731 Vibes</span>.
 					Book your teeth whitening session today and embark on a journey to a more vibrant,
 					confident you. Radiate positivity with a luminous smile that leaves a lasting impression.</span
 				>
@@ -217,37 +217,37 @@
 			<p>Discover the benefits of choosing our body waxing services:</p>
 
 			<p>
-				<span class="font-light pr-1">Professional Precision:</span> Trust in the expertise of our skilled
-				estheticians who bring professionalism and precision to every waxing session, ensuring optimal
-				results with minimal discomfort.
+				<span class="font-semibold pr-1">Professional Precision:</span> Trust in the expertise of our
+				skilled estheticians who bring professionalism and precision to every waxing session, ensuring
+				optimal results with minimal discomfort.
 			</p>
 
 			<p>
-				<span class="font-light pr-1">Quality Waxing Products:</span> We use top-quality, gentle waxing
+				<span class="font-semibold pr-1">Quality Waxing Products:</span> We use top-quality, gentle waxing
 				products that cater to different skin types, providing a smooth and effective hair removal experience
 				without compromising your skin's health.
 			</p>
 
 			<p>
-				<span class="font-light pr-1">Lasting Smoothness:</span> Experience long-lasting smoothness as
-				our body waxing technique removes hair from the root, delaying regrowth and leaving your skin
+				<span class="font-semibold pr-1">Lasting Smoothness:</span> Experience long-lasting smoothness
+				as our body waxing technique removes hair from the root, delaying regrowth and leaving your skin
 				silky and hair-free for an extended period.
 			</p>
 
 			<p>
-				<span class="font-light pr-1">Customized Waxing Options:</span> Tailor your waxing experience
+				<span class="font-semibold pr-1">Customized Waxing Options:</span> Tailor your waxing experience
 				to your preferences with our range of customized waxing options. From full-body waxing to specific
 				areas, we offer flexibility to meet your unique grooming needs.
 			</p>
 
 			<p>
-				<span class="font-light pr-1">Comfortable Environment:</span> Relax in a comfortable and hygienic
+				<span class="font-semibold pr-1">Comfortable Environment:</span> Relax in a comfortable and hygienic
 				environment designed to make your waxing experience as pleasant as possible. Our spa setting
 				creates a serene atmosphere for a stress-free session.
 			</p>
 
 			<p>
-				<span class="font-light pr-1">Efficiency and Time-Saving:</span> Our efficient waxing process
+				<span class="font-semibold pr-1">Efficiency and Time-Saving:</span> Our efficient waxing process
 				ensures a quick and time-saving experience, perfect for those with busy schedules who want to
 				achieve smooth, hair-free skin without sacrificing their valuable time.
 			</p>
@@ -255,8 +255,8 @@
 			<p>
 				<span
 					>Revel in the luxury of smooth skin and the confidence it brings. Book your body waxing
-					session at <span class="font-light">731 Vibes</span> and step into a world where beauty meets
-					precision. Unveil a more confident and radiant you with our expert body waxing services.</span
+					session at <span class="font-semibold">731 Vibes</span> and step into a world where beauty
+					meets precision. Unveil a more confident and radiant you with our expert body waxing services.</span
 				>
 			</p>
 
