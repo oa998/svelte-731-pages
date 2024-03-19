9557940663acd3c599fc6dca5966de6a1922b8f6
zzz sss <onlineaccounts998@gmail.com>
2024-03-19 18:09:55 -0400

    auto commit

--- src/routes/(everything-else)/(max-w-2xl)/specials/easter-egg/+page.svelte
+++ src/routes/(everything-else)/(max-w-2xl)/specials/easter-egg/+page.svelte
@@ -4,13 +4,13 @@
 <div class="text-white flex flex-col gap-5">
 	<div class="text-3xl sm:text-5xl font-serif text-center font-extralight">Easter Egg My Yard!</div>
 
-	<img src="/eemy.jpeg" alt="easter-egg-my-yard" class=" w-full max-w-lg m-auto" />
+	<img src="/easter-2.jpeg" alt="easter-egg-my-yard" class=" w-full max-w-lg m-auto" />
 
 	<h1 class="pb-2 text-2xl sm:text-5xl font-sans font-thin text-center">Sign up below:</h1>
 	<div class="flex flex-col sm:flex-row justify-around items-center gap-3">
 		<a target="_blank" href={'https://buy.stripe.com/dR67vD0D0dR54JG7sx'}>50 Eggs - $40</a>
-		<a target="_blank" href={'https://buy.stripe.com/3cs3fngBY7sHfok6os'}>70 Eggs - $50</a>
-		<a target="_blank" href={'https://buy.stripe.com/14kcPX4Tg28nekg8wz'}>100 Eggs - $75</a>
+		<a target="_blank" href={'https://buy.stripe.com/3cs3fngBY7sHfok6os'}>70 Eggs - $60</a>
+		<a target="_blank" href={'https://buy.stripe.com/14kcPX4Tg28nekg8wz'}>100 Eggs - $80</a>
 	</div>
 </div>
 
